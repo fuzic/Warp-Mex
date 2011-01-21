ab532da97833c5cabfbfb530d5364db17fc839d1
fuzic <echo@mit.edu>
2011-01-21 14:28:35 -0500

    removed mex lib dependencies, no differences

--- simpleEdge3d_l.cpp
+++ simpleEdge3d_l.cpp
@@ -1,6 +1,3 @@
-#include <math.h>
-#include "mex.h"
-#include "matrix.h"
 #include "num_comps_edge.h"
 
 static int patchsize[4] = {3,3,3,3};
@@ -34,7 +31,7 @@ bool simpleEdge3d(bool *orig_graphPatch, unsigned int v, unsigned int fg_conn)
 	bool simple;
 	bool *graphPatch;
 	
-	graphPatch=(bool *)mxCalloc(81,sizeof(bool));
+	graphPatch=(bool *)calloc(81,sizeof(bool));
 	
 	for(i=0;i<81;i++)
 		graphPatch[i]=(fg_conn==6)?orig_graphPatch[i]:!orig_graphPatch[i];
@@ -45,7 +42,7 @@ bool simpleEdge3d(bool *orig_graphPatch, unsigned int v, unsigned int fg_conn)
 	int T2=NCCNh2(graphPatch,v);
 	
 	simple=(T1 == 1 && T2 ==1)?true:false;
-	mxFree(graphPatch);
+	free(graphPatch);
 	
 	return simple;
 }
